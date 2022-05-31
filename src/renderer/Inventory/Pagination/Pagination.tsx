875b65e56a5b6e08d2bd55349638578f799694eb
Tim <wongtimothy13@gmail.com>
2022-05-30 23:46:27 -0500

    pagination

--- src/renderer/Inventory/Pagination/Pagination.tsx
+++ src/renderer/Inventory/Pagination/Pagination.tsx
@@ -1,6 +1,9 @@
 import { max } from 'lodash';
 import * as React from 'react';
 import './pagination.css';
+import left_white_arrow from "./../../../../assets/generalIcons/left_arrow_white.png";
+import right_white_arrow from "./../../../../assets/generalIcons/right_arrow_white.png";
+
 
 const Pagination = (props:any) => {
   const {
@@ -8,19 +11,27 @@ const Pagination = (props:any) => {
     current
   } = props;
 
-
+  const size = 25;
 
   return (
     <>
-      <div className="squares_wrapper">
-        { (max <= 5) && [...Array(max)].map((val, index) => {
-            return (
-              <div className="square_index">
-                {index + 1}
-              </div>
-            )
-          })
-        }
+      <div className="pagination_wrapper">
+        <div className="pagination_left_arrow"> 
+          <img height={size} width={size} src={left_white_arrow} draggable="false" />
+        </div>
+        <div className="squares_wrapper">
+          { [...Array(max)].map((val, index) => {
+              return (
+                <div className={`square_index ${current == index + 1 ? "selected_page" : ""}`}>
+                  {index + 1}
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="pagination_right_arrow"> 
+          <img height={size} width={size} src={right_white_arrow} draggable="false" />
+        </div>
       </div>
       
     </>
